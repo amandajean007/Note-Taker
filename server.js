b02cb75624ee8eda7c615ccff33282e921e8c7fb
Amanda McBee <ahanes35@yahoo.com>
2021-09-10 13:42:45 -0600

    delete/ish, select, new, savenote/ish, most works

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public/'));
 
+
+
+
+
+
+
+
 // The home page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -24,11 +31,16 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
 // GET request for notes
 app.get('/api/notes', (req, res) => {
-    res.status(200).json(notes);
-  });
+  res.status(200).json(notes)
+});
+
+
+
+
+
+
 
 
 // POST request to add a note
@@ -46,21 +58,80 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
-  
+
+   // Obtain existing notes
+   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      // Add a new note
+      parsedNotes.push(newNote);
+
+      // Write updated notes back to the file
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes, null, 3),
+        (err) =>
+          err
+            ? console.error(err)
+            : console.info('Successfully added note!')
+      );
+    }
+  });
+
+
       const response = {
         status: 'success',
         body: newNote,
       };
   
       console.log(response);
-      res.status(201).json(response);
+     res.status(201).json(response);
     } else {
       res.status(500).json('Error in posting note');
     }
   });
   
+
+  // Delete an note
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (notes.id !== 0) {
+    console.log(id);
+    notes.splice(id, 1);
+          // Write updated notes back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(notes, null, 3),
+            (err) =>
+              err
+                ? console.error(err)
+                : console.info('Successfully deleted note!')
+          );
+    res.status(204).send();
+  } else {
+    res.status(405).send();
+  }
+
+
+
+//   app.delete('/notes/:id', (req, res) => {
+//     notes
+//         .removeNote(req.params.id)
+//         .then(() => res.json({ ok: true }))
+//         .catch(err => res.status(500).json(err))
+// })
+
+  
+ });
+
+
+
   app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
   );
