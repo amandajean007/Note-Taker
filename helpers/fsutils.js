ed8765fa735aad6231d62c97b2a33b3bdb553dc1
Amanda McBee <ahanes35@yahoo.com>
2021-11-10 13:27:44 -0700

    reorganized files, made routes folder and  fs folder

---
+++ helpers/fsutils.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeToFile = (destination, content) => {
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+};
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const deleteNote = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      const allButDeletedNote = parsedData.filter((note) => note.id != content);
+      writeToFile(file, allButDeletedNote);
+    }
+  });
+};
+
+module.exports = { readAndAppend, writeToFile, deleteNote };
